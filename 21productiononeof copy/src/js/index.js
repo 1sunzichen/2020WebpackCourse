ce0675d4a582ccac96bbfa7981c237608aa10214
“sjq” <3023493319@qq.com>
2020-04-02 10:26:55 +0800

    20oneof=》中的文件只匹配一种loader

---
+++ 21productiononeof copy/src/js/index.js
@@ -0,0 +1,14 @@
+//import '@babel/polyfill';
+const add = function add(x, y) {
+  return x + y;
+};
+
+
+const promises =new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("suc")
+  }, 1000);
+})
+console.log('====================================');
+console.log(add(2, 5),promises);
+console.log('====================================');
